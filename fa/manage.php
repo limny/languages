92a8b8aa71433f34e8d65f7c1a08f593b83bb183
Hamid Samak <hamidreza.samak@gmail.com>
2015-10-21 00:14:38 +0330

    Added Farsi language

---
+++ fa/manage.php
@@ -0,0 +1,24 @@
+<?php
+
+def('ADD', 'افزودن');
+def('SEARCH', 'جستجو');
+def('DELETE_SELECTED', 'حذف انتخاب شده‌ها');
+def('BACK', 'بازگشت');
+def('DELETE', 'حذف');
+def('EDIT', 'ویرایش');
+def('VIEW', 'مشاهده');
+def('ADD_MORE', 'افزودن بیشتر');
+def('YES', 'بله');
+def('NO', 'خیر');
+def('ITEMS_IN', 'مورد در');
+def('PAGES', 'صفحه');
+
+def('MANAGE_SENTENCE_1', 'هیچ مورد پیدا نشد.');
+def('MANAGE_SENTENCE_2', 'لطفا فیلدهای ضروری را پر کنید.');
+def('MANAGE_SENTENCE_3', 'آیا مطمئن هستید می‌خواهید {NUMBER} مورد را حذف کنید؟');
+def('MANAGE_SENTENCE_4', 'مورد جدید با موفقیت اضافه شد.');
+def('MANAGE_SENTENCE_5', 'مورد با موفقیت ویرایش شد.');
+def('MANAGE_SENTENCE_6', 'آیتم(ها) با موفقیت حذف شدند.');
+def('MANAGE_SENTENCE_7', 'فایل پیوست شده تصویر نیست.');
+
+?>
